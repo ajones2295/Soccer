8ca6a7737addc585fd2cd2b009697199c87ea5c1
ajones2295 <132492414+ajones2295@users.noreply.github.com>
2023-08-07 22:37:52 -0500

    changes to temp

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,5 @@
 /**
-* Template Name: Gp
+* Template Name: Coach
 * Updated: May 30 2023 with Bootstrap v5.3.0
 * Template URL: https://bootstrapmade.com/gp-free-multipurpose-html-bootstrap-template/
 * Author: BootstrapMade.com
